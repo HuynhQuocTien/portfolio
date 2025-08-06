86270ef21b9f6a240b57cd46874c077c01bae55b
HUỲNH QUỐC TIẾN <115643869+HuynhQuocTien@users.noreply.github.com>
2025-08-07 01:09:12 +0700

    Update homepage URL in header object to reflect new portfolio site

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://huynhquoctien.github.io/portfolio',
+  homepage: 'https://zenithnode.vercel.app/',
   title: 'JS.',
 }
 
